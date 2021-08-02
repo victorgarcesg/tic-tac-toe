8a25e327c8bf69f2a4b8e0c742f0f422c2899b06
Victor Garces <victorgarcesgg@gmail.com>
2021-08-02 14:32:23 -0400

    Refactored Board component

--- src/index.js
+++ src/index.js
@@ -22,25 +22,19 @@ class Board extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+        let counter = 0;
+        let boardRows = [];
+        for(let i = 0; i < 3; i++) {
+            let rowSquares = [];
+            for(let j = 0; j < 3; j++) {
+                rowSquares.push(
+                    this.renderSquare(counter)
+                );
+                counter += 1;
+            }
+            boardRows.push(<div className="board-row">{rowSquares}</div>);
+        }
+        return <div>{boardRows}</div>;
     }
 }
 
